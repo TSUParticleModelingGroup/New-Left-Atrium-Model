163eddf965e4cfde83c3998f715764522c7df601
MBane04 <mbane0525@gmail.com>
2025-04-27 12:14:01 -0500

    Added previous runs file, added gedit to dependencies

--- dependencies.sh
+++ dependencies.sh
@@ -4,6 +4,7 @@
 # Install system packages
 echo "Installing system dependencies..."
 sudo apt update && sudo apt upgrade -y
+sudo apt install gedit -y
 sudo apt install libglfw3-dev libx11-dev libxrandr-dev libxinerama-dev libxcursor-dev libxi-dev libxext-dev libxfixes-dev libgl1-mesa-dev libglu1-mesa-dev libsoil-dev
 sudo apt install mesa-utils -y
 sudo apt install freeglut3-dev -y
